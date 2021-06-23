760bdf74f7a3acb78ce8f355b3e662edb5169833
Rakesh Chalasani <vnit.rakesh@gmail.com>
2021-06-22 23:22:37 -0400

    Add matting and start with adding backgrounds

--- background/person_detection.py
+++ background/person_detection.py
@@ -7,12 +7,17 @@ import argparse
 
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
 
 import numpy as np
+from PIL import Image
 
 from torchvision.transforms import Compose
+import torchvision.transforms as transforms
 from dpt.models import DPTSegmentationModel
 from dpt.transforms import Resize, NormalizeImage, PrepareForNet
+
+from src.models.modnet import MODNet
 
 
 class PersonDetection():
@@ -91,17 +96,102 @@ class PersonDetection():
 
         return prediction
 
+    def __modnet_prediction(self):
 
-    def detect(self):
+        # select device
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # define hyper-parameters
+        ref_size = 512
+
+        # define image to tensor transform
+        im_transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            ]
+        )
+
+        model_path = './MODNet/pretrained/modnet_photographic_portrait_matting.ckpt'
+
+        # create MODNet and load the pre-trained ckpt
+        modnet = MODNet(backbone_pretrained=False)
+        if device == torch.device("cuda"):
+            modnet = nn.DataParallel(modnet).cuda()
+            modnet.load_state_dict(torch.load(model_path, map_location = device))
+        else:
+            state_dict = torch.load(model_path, map_location = device)
+
+            from collections import OrderedDict
+            new_state_dict = OrderedDict()
+            for k, v in state_dict.items():
+                name = k[7:] # remove 'module.' of dataparallel
+                new_state_dict[name]=v
+
+            modnet.load_state_dict(new_state_dict)
+
+        modnet.eval()
+
+        modnet.to(device)
+
+        # unify image channels to 3
+        im = np.asarray(self.image * 255.0).astype(np.uint8)
+        if len(im.shape) == 2:
+            im = im[:, :, None]
+        if im.shape[2] == 1:
+            im = np.repeat(im, 3, axis=2)
+        elif im.shape[2] == 4:
+            im = im[:, :, 0:3]
+
+        # convert image to PyTorch tensor
+        im = Image.fromarray(im)
+        im = im_transform(im)
+
+        # add mini-batch dim
+        im = im[None, :, :, :]
+
+        # resize image for input
+        im_b, im_c, im_h, im_w = im.shape
+        if max(im_h, im_w) < ref_size or min(im_h, im_w) > ref_size:
+            if im_w >= im_h:
+                im_rh = ref_size
+                im_rw = int(im_w / im_h * ref_size)
+            elif im_w < im_h:
+                im_rw = ref_size
+                im_rh = int(im_h / im_w * ref_size)
+        else:
+            im_rh = im_h
+            im_rw = im_w
+        
+        im_rw = im_rw - im_rw % 32
+        im_rh = im_rh - im_rh % 32
+        im = F.interpolate(im, size=(im_rh, im_rw), mode='area')
+
+        # inference
+        _, _, matte = modnet(im, True)
+
+        # resize and save matte
+        matte = torch.clamp(F.interpolate(matte, size=(im_h, im_w), mode='area'), 0, 1)
+        matte = matte[0][0].data.cpu().numpy()
+
+        return matte
+
+
+    def detect(self, model_type = 'dpt'):
         """
         Run segmentation network
         """
         print("Personal detection - initializing")
+        if model_type == 'dpt':
 
-        dpt_prediction_hybrid = self.__dpt_prediction(dpt_model_type='dpt_hybrid', optimize=True)
-        dpt_prediction_large = self.__dpt_prediction(dpt_model_type='dpt_large', optimize=True)
+            dpt_prediction_hybrid = self.__dpt_prediction(dpt_model_type='dpt_hybrid', optimize=True)
+            dpt_prediction_large = self.__dpt_prediction(dpt_model_type='dpt_large', optimize=True)
 
-        return np.minimum(dpt_prediction_large, dpt_prediction_hybrid)
+            return np.minimum(dpt_prediction_large, dpt_prediction_hybrid)
 
-        
+        if model_type == 'modnet':
+
+            return self.__modnet_prediction()
+
+        return None
 
--- image_merging/matting.py
+++ image_merging/matting.py
@@ -4,19 +4,42 @@ import numpy as np
 # Given a binary segmentation map, cuts or replace the background while preserving foreground. Doesn't use any matting techniluqes.
 class ImageCut():
 
-    def __init__(self, image, background) -> None:
+    def __init__(self, image, mask, background = None) -> None:
         self.image = image
+        self.mask = mask # mask {0, 1} determing foreground vs background.
         self.background = background # Optional parameter, passing Non equivalent to remove background. 
 
     # Cut background and return on foreground objects. 
     def cut(self):
-        out_img = np.ones(self.image.shape,dtype=self.image.dtype)*255
+        if self.background:
+            out_img = self.background
+        else: 
+            out_img = np.ones(self.image.shape,dtype=self.image.dtype)*255
         
-        out_img = np.where(np.repeat(self.background[..., None],3,axis=2) == 1, self.image, out_img)
+        out_img = np.where(np.repeat(self.mask[..., None],3,axis=2) == 1, self.image, out_img)
 
         return out_img
 
 
+
+class AlphaMatting():
+
+    def __init__(self, image, alpha, background = None) -> None:
+        self.image = image
+        self.alpha = alpha
+        self.background = background # Optional parameter, passing None equivalent to remove background. 
+
+    # Cut background and return on foreground objects. 
+    def cut(self):
+        if self.background:
+            out_img = self.background
+        else:
+            out_img = np.ones(self.image.shape,dtype=self.image.dtype)*255
+
+        tiled_alpha = np.repeat(self.alpha[..., None],3,axis=2)
+        out_img = np.multiply(tiled_alpha, self.image) + np.multiply((1 - tiled_alpha), out_img)
+
+        return out_img
 
 
 
